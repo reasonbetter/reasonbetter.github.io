9162db210910155176968dcf1e70c7cb776c185c
David <dmanley8@gmail.com>
2025-09-20 21:12:01 -0400

    d

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'export',               // puts the static site in ./out
-  images: { unoptimized: true },  // required for static export
-};
+  output: 'export',               
+  images: { unoptimized: true },  
