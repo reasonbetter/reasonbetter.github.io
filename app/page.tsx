20131835c9ff29a502a5a5b65753861473b1d4d3
David <dmanley8@gmail.com>
2025-09-22 10:39:45 -0400

    book a meeting

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
                   <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
                 </svg>
                 <a href="https://calendar.app.google/huGbAtsbdEhxYGRh8" target="_blank" rel="noreferrer" className="ml-3 text-sm text-slate-700 dark:text-slate-300 hover:underline">
-                  Book a Meeting
+                  Book a meeting
                 </a>
               </div>
               <div className="flex items-center">
@@ -322,6 +322,12 @@ export default function Home() {
                 </svg>
                 <a href="mailto:dmanley@umich.edu" className="ml-3 text-sm font-medium text-slate-800 dark:text-slate-200 hover:underline">
                   dmanley@umich.edu
+                </a>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-slate-500 ml-6" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
+                </svg>
+                <a href="https://calendar.app.google/huGbAtsbdEhxYGRh8" target="_blank" rel="noreferrer" className="ml-3 text-sm font-medium text-slate-800 dark:text-slate-200 hover:underline">
+                  Book a meeting
                 </a>
               </div>
             </div>
